879851451d61b45ea97b293784e1bc061bf87b52
sunderpradeep14-blip <sunderpradeep14@gmail.com>
2026-07-11 22:20:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -68,4 +68,5 @@ def records():
     return render_template('records.html', records=records)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT",5000))
+    app.run(host="0.0.0.0.",port=port)
